87fc2142e3515f09b7b550c1314b714c49dd518f
Yaksh Yadav <yaksh25csu267@ncuindia.edu>
2025-11-10 11:38:50 +0530

    Q8.c

---
+++ Q8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() 
+{
+    int n,t1=0,t2=1,nextTerm=0;
+    printf("Enter the number of terms: ");
+    scanf("%d",&n);
+    printf("Fibonacci Series: ");
+    for (int i=1;i<=n;++i) 
+    {
+        printf("%d ",t1);
+        nextTerm=t1+t2;
+        t1=t2;
+        t2=nextTerm;
+    }
+    printf("\n");
+    return 0;
+}
